b6de47df3803863a1de45a419e31192b070b4968
naviholloway <aholloway@navisite.com>
2025-03-20 10:40:34 -0400

    First Commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+
+BACKGROUND_COLOR = (0, 0, 0)
+
+def main():
+    pygame.init()
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while pygame.get_init():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(BACKGROUND_COLOR)
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
