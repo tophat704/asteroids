9a1245a0c2a696318374d79226e401d89d161815
naviholloway <aholloway@navisite.com>
2025-03-22 11:09:11 -0400

    Add Player

--- main.py
+++ main.py
@@ -3,21 +3,27 @@
 # throughout this file
 import pygame
 from constants import *
-
-BACKGROUND_COLOR = (0, 0, 0)
+from player import *
 
 def main():
     pygame.init()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    start_x = SCREEN_WIDTH / 2
+    start_y = SCREEN_HEIGHT / 2
+    player = Player(start_x, start_y)
     while pygame.get_init():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(BACKGROUND_COLOR)
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
